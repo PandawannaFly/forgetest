d6bd12711977e8f3c2e696f1c716a3da45aa6783
pandawannafly <ezioha115@gmail.com>
2022-10-20 16:30:03 +0700

    fixingg

--- frontend/src/component/Body/ViewForge/ForgeView.js
+++ frontend/src/component/Body/ViewForge/ForgeView.js
@@ -1,115 +1,91 @@
 /* eslint-disable no-undef */
-import { getForgeToken } from 'untils/request';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-// Extensions
-// import MyExtension from 'Extensions/MyExtension/MyExtension';
-import ExplodeExtension from 'Extensions/ExplodeExtension/ExplodeExtension';
-// import TurnTableExtension from 'Extensions/CameraRotation/CameraRotation';
-// import MarkUp3DExtension from 'Extensions/MarkUp3DExtension/MarkUp3DExtension';
-// import DrawToolExtension from 'Extensions/DrawToolExtension/DrawToolExtension';
-// import IconMarkupExtension from 'Extensions/IconMarkupExtension/IconMarkupExtension';
-// import NestedViewerExtension from 'Extensions/NestedViewerExtension/NestedViewerExtension';
-// import ModelSummaryExtension from 'Extensions/ModelSummaryExtension/ModelSummaryExtension';
-// import RegisterTransformTool from 'Extensions/TransformationExtension/TransformationExtension';
-// import HandleSelectionExtension from 'Extensions/HandleSelectionExtension/HandleSelectionExtension';
+import { getForgeToken } from "untils/request";
+import { urnSelector } from "redux/UrnLink/urnSelcetor";
+import { bootScreenSelector } from "redux/Refresh/refreshSelector";
+
 
 let viewer;
 
-const extensions = [
-    // 'MyExtension',
-    // 'TurnTable',
-    'ExplodeExtension',
-    // 'MarkUp3DExtension',
-    // 'DrawToolExtension',
-    // 'IconMarkupExtension',
-    // 'NestedViewerExtension',
-    // 'ModelSummaryExtension',
-    // 'TransformationExtension',
-    // 'HandleSelectionExtension',
-];
+const extensions =[
+  'HandleSelectionExtension',
+]
 
+//load file len viewer
 function onDocumentLoadSuccess(doc) {
-    let viewables = doc.getRoot().getDefaultGeometry();
+  let viewables = doc.getRoot().getDefaultGeometry();
 
-    viewer.loadDocumentNode(doc, viewables).then((i) => {
-        console.log('load document success');
-    });
+  viewer.loadDocumentNode(doc, viewables).then((i) => {
+    // document loaded, them action ?
+  });
 }
 
 function onDocumentLoadFailure(viewerErrorCode, viewerErrorMsg) {
-    console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode + '\n- errorMessage:' + viewerErrorMsg);
+  console.error(
+    "onDocumentLoadFailure() - errorCode:" +
+      viewerErrorCode +
+      "\n- errorMessage:" +
+      viewerErrorMsg
+  );
 }
 
-function launchViewer(urn) {
-    if (urn !== null) {
-        const options = {
-            env: 'AutodeskProduction',
-            getAccessToken: getForgeToken,
-        };
+export function launchViewer(urn) {
+  if (urn !== null) {
+    const options = {
+      env: "AutodeskProduction",
+      getAccessToken: getForgeToken,
+    };
+// co the them moi cac extension vao day
+    Autodesk.Viewing.Initializer(options, () => {
 
-        Autodesk.Viewing.Initializer(options, () => {
-            //==Register Extension==//
+      
+      viewer = new Autodesk.Viewing.GuiViewer3D(
+        document.getElementById("forgeViewer"),
+        {
+          //
+          extensions: extensions,
+        }
 
-            // //Camera Rotation
-            // Autodesk.Viewing.theExtensionManager.registerExtension('TurnTable', TurnTableExtension);
 
-            // Mark Up 3D Extension
-            // Autodesk.Viewing.theExtensionManager.registerExtension('MarkUp3DExtension', MarkUp3DExtension);
-
-            // //Draw Tool Extension
-            // Autodesk.Viewing.theExtensionManager.registerExtension('DrawToolExtension', DrawToolExtension);
-
-            // //Icon Markup Extension
-            // Autodesk.Viewing.theExtensionManager.registerExtension('IconMarkupExtension', IconMarkupExtension);
-
-            // //NestedViewerExtension
-            // Autodesk.Viewing.theExtensionManager.registerExtension('NestedViewerExtension', NestedViewerExtension);
-
-            // //Model Summary Extension
-            // Autodesk.Viewing.theExtensionManager.registerExtension('ModelSummaryExtension', ModelSummaryExtension);
-
-            //Transformation Extension
-            // RegisterTransformTool();
-
-            // //Handle Selection Extension
-            // Autodesk.Viewing.theExtensionManager.registerExtension(
-            //     'HandleSelectionExtension',
-            //     HandleSelectionExtension,
-            // );
-
-            // //Popout Extension
-            Autodesk.Viewing.theExtensionManager.registerExtension('ExplodeExtension', ExplodeExtension);
-
-            // My Extension
-            // Autodesk.Viewing.theExtensionManager.registerExtension('MyExtension', MyExtension);
-
-            //========================================================//
-
-            viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), {
-                extensions: extensions,
-            });
-
-            viewer.setDisplayEdges(true);
-            // viewer.loadExtension('Autodesk.BIM360.GestureDocumentNavigation');
-            viewer.loadExtension('Autodesk.Viewing.Popout');
-
-            viewer.loadExtension('Autodesk.MemoryLimited');
-            viewer.loadExtension('Autodesk.Viewing.MemoryLimitedDebug');
-
-            viewer.start();
-            let documentId = 'urn:' + urn;
-            Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
-        });
-    } else if (urn === null) {
-        extensions.map((item) => {
-            Autodesk.Viewing.theExtensionManager.unregisterExtension(item);
-            return true;
-        });
-
-        viewer.finish();
-        viewer = null;
-        Autodesk.Viewing.shutdown();
-    }
+        );
+        Autodesk.Viewing.theExtensionManager.registerExtension('HandleSelectionExtension',
+        HandleSelectionExtension);
+     
+      viewer.start();
+      let documentId = "urn:" + urn;
+      Autodesk.Viewing.Document.load(
+        documentId,
+        onDocumentLoadSuccess,
+        onDocumentLoadFailure
+      );
+    });
+  }
 }
 
-export default launchViewer;
+export  function ViewContainer() {
+  const bootScreen = useSelector(bootScreenSelector);
+
+  const urn = useSelector(urnSelector);
+
+  useEffect(() => {
+    launchViewer(urn);
+  }, [urn]);
+
+  return (
+    <>
+      {bootScreen ? (
+        <img
+          src="https://aecom.solutions/wp-content/uploads/2020/12/blo.jpg"
+          alt="Boot Screen to change the world!"
+          style={{ width: "100%", height: "100%", opacity: "0.85" }}
+        />
+      ) : (
+        <div id="forgeViewer"></div>
+      )}
+    </>
+  );
+}
+
+export default ViewContainer;
